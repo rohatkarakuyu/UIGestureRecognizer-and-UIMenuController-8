261b94b489b010b24ea65139de340e9729a055d7
deenmeefe <eddededeed>
2020-03-12 20:33:33 -0700

    Initial Commit

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Rohat Karakuyu on 5.03.2020.
+//  Copyright © 2020 RohatiOS. All rights reserved.
+//
+
+import Foundation
+
+import CoreGraphics
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
+
---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Rohat Karakuyu on 5.03.2020.
+//  Copyright © 2020 RohatiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
